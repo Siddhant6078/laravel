4ba72a141678422300610a1db248b4dff5cec6e3
Siddhant Chaudhari <siddhantschaudhari@gmail.com>
2018-07-04 13:46:18 +0530

    host

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'heroku_0777aef68ad8e77'),
             'username' => env('DB_USERNAME', 'bf26f43b435880'),
