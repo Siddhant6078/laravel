d69b9832d63496b6c20f7adbda6f4519f8f8b105
Siddhant Chaudhari <siddhantschaudhari@gmail.com>
2018-07-04 14:19:34 +0530

    login

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,6 +4,6 @@
 <div class="jumbotron text-center">
     <h1>{{$title}}</h1>
     <p>This is the Laravel Application from the "Laravel From Scratch" YouTube series</p>
-    <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+    <p><a class="btn btn-primary btn-lg" href="{{ route('login') }}" role="button">Login</a> <a class="btn btn-success btn-lg" href="{{ route('register') }}" role="button">Register</a></p>
 </div>
 @endsection
